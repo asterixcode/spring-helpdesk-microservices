de8182a3a2e6190ee2f43bc6fc6afec662afdf87
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-11 20:58:51 +0200

    chore: start user-service-api

---
+++ user-service-api/src/test/java/com/asterixcode/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asterixcode.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
