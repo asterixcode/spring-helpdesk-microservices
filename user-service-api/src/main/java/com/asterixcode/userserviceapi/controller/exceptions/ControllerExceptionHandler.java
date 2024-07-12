e103ce3976cf1902929dbe4e51f66a365a0d8e8f
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-12 19:23:33 +0200

    feat: add validation for email already exists

--- user-service-api/src/main/java/com/asterixcode/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
+++ user-service-api/src/main/java/com/asterixcode/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
@@ -1,14 +1,14 @@
 package com.asterixcode.userserviceapi.controller.exceptions;
 
 import static java.time.LocalDateTime.now;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import models.exceptions.ResourceNotFoundException;
 import models.exceptions.StandardError;
 import models.exceptions.ValidationError;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,21 +20,35 @@ public class ControllerExceptionHandler {
 
   @ExceptionHandler(ResourceNotFoundException.class)
   ResponseEntity<StandardError> handleResourceNotFoundException(
-      final ResourceNotFoundException e, final HttpServletRequest request) {
+      final ResourceNotFoundException ex, final HttpServletRequest request) {
     return ResponseEntity.status(NOT_FOUND)
         .body(
             StandardError.builder()
                 .timestamp(now())
                 .status(NOT_FOUND.value())
                 .error(NOT_FOUND.getReasonPhrase())
-                .message(e.getMessage())
+                .message(ex.getMessage())
                 .path(request.getRequestURI())
                 .build());
   }
 
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  ResponseEntity<StandardError> handleDataIntegrityViolationException(
+          final DataIntegrityViolationException ex, final HttpServletRequest request) {
+    return ResponseEntity.status(CONFLICT)
+            .body(
+                    StandardError.builder()
+                            .timestamp(now())
+                            .status(CONFLICT.value())
+                            .error(CONFLICT.getReasonPhrase())
+                            .message(ex.getMessage())
+                            .path(request.getRequestURI())
+                            .build());
+  }
+
   @ExceptionHandler(MethodArgumentNotValidException.class)
   ResponseEntity<ValidationError> handleMethodArgumentNotValidException(
-      final MethodArgumentNotValidException e, final HttpServletRequest request) {
+      final MethodArgumentNotValidException ex, final HttpServletRequest request) {
     var errors =
         ValidationError.builder()
             .timestamp(now())
@@ -45,7 +59,7 @@ public class ControllerExceptionHandler {
             .errors(new ArrayList<>())
             .build();
 
-    for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+    for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
       errors.addError(fieldError.getField(), fieldError.getDefaultMessage());
     }
 
--- user-service-api/src/main/java/com/asterixcode/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/com/asterixcode/userserviceapi/service/UserService.java
@@ -5,6 +5,7 @@ import com.asterixcode.userserviceapi.repository.UserRepository;
 import models.exceptions.ResourceNotFoundException;
 import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,13 +23,28 @@ public class UserService {
     return userMapper.fromEntity(
         userRepository
             .findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException(
-                    "Object not found. Id: " + id + ", Type: " + UserResponse.class.getSimpleName()
-            ))
-    );
+            .orElseThrow(
+                () ->
+                    new ResourceNotFoundException(
+                        "Object not found. Id: "
+                            + id
+                            + ", Type: "
+                            + UserResponse.class.getSimpleName())));
   }
 
   public void save(CreateUserRequest createUserRequest) {
+    verifyIfEmailExists(createUserRequest.email(), null);
     userRepository.save(userMapper.fromRequest(createUserRequest));
+  }
+
+  private void verifyIfEmailExists(final String email, final String id) {
+    userRepository
+        .findByEmail(email)
+        .filter(user -> !user.getId().equals(id))
+        .ifPresent(
+            user -> {
+              throw new DataIntegrityViolationException(
+                  "Email [ %s ] already exists".formatted(email));
+            });
   }
 }
