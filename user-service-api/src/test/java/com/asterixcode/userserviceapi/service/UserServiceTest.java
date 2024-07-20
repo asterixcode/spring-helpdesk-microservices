bcb97112170d70cf5092417e7368827a74062526
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-20 20:22:44 +0200

    test: add find by id with invalid id in user service

--- user-service-api/src/test/java/com/asterixcode/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/com/asterixcode/userserviceapi/service/UserServiceTest.java
@@ -9,6 +9,7 @@ import com.asterixcode.userserviceapi.entity.User;
 import com.asterixcode.userserviceapi.mapper.UserMapper;
 import com.asterixcode.userserviceapi.repository.UserRepository;
 import java.util.Optional;
+import models.exceptions.ResourceNotFoundException;
 import models.responses.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -45,5 +46,20 @@ class UserServiceTest {
 
     verify(repository, times(1)).findById(anyString());
     verify(mapper, times(1)).fromEntity(any(User.class));
+  }
+
+  @Test
+  void whenCallFindByIdWithInvalidIdThenThrowResourceNotFoundException() {
+    when(repository.findById(anyString())).thenReturn(Optional.empty());
+
+    try{
+      service.findById("1");
+    } catch (Exception e) {
+      assertEquals(ResourceNotFoundException.class, e.getClass());
+      assertEquals("Object not found. Id: 1, Type: UserResponse", e.getMessage());
+    }
+
+    verify(repository, times(1)).findById(anyString());
+    verify(mapper, never()).fromEntity(any(User.class));
   }
 }
