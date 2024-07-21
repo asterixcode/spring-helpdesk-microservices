6b1a68712ed4dbfaaae1123a24e37d51576c239e
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-21 17:43:31 +0200

    feat: add security config

---
+++ auth-service-api/src/main/java/com/asterixcode/authserviceapi/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.asterixcode.authserviceapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            authorizeHttp -> {
+              authorizeHttp.requestMatchers("/swagger-ui/**").permitAll();
+              authorizeHttp.requestMatchers("/swagger-ui.html").permitAll();
+              authorizeHttp.requestMatchers("/v3/api-docs/**").permitAll();
+              authorizeHttp.requestMatchers("/api/v1/auth/login/**").permitAll();
+              authorizeHttp.anyRequest().authenticated();
+            })
+        .sessionManagement(AbstractHttpConfigurer::disable)
+        .build();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
