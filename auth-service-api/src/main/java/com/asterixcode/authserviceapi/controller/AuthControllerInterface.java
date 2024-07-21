66aa5f7370e729a00dd4d124d73a5d4bc5c1c89b
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-21 17:25:48 +0200

    feat: add authN endpoint contract

---
+++ auth-service-api/src/main/java/com/asterixcode/authserviceapi/controller/AuthControllerInterface.java
@@ -0,0 +1,58 @@
+package com.asterixcode.authserviceapi.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import models.exceptions.StandardError;
+import models.requests.AuthenticationRequest;
+import models.responses.AuthenticationResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/auth")
+public interface AuthControllerInterface {
+
+  @Operation(summary = "Authenticate user")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "User authenticated",
+            content =
+                @Content(
+                    mediaType = APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = AuthenticationResponse.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad request",
+            content =
+                @Content(
+                    mediaType = APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = StandardError.class))),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Bad Credentials",
+            content =
+                @Content(
+                    mediaType = APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = StandardError.class))),
+        @ApiResponse(
+            responseCode = "404",
+            description = "User not found",
+            content =
+                @Content(
+                    mediaType = APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = StandardError.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+      })
+  @PostMapping("/login")
+  ResponseEntity<AuthenticationResponse> authentication(
+      @Valid @RequestBody final AuthenticationRequest request);
+}
---
+++ hd-commons-lib/src/main/java/models/responses/AuthenticationResponse.java
@@ -0,0 +1,6 @@
+package models.responses;
+
+import lombok.Builder;
+
+@Builder
+public record AuthenticationResponse(String token, String type) {}
