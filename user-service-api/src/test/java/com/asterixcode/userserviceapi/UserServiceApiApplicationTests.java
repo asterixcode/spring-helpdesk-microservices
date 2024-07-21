46cb21924b4cd0a86a34e6cb482a235f5d1c67c4
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-21 13:21:59 +0200

    test: use testcontainers for contextloads test

--- user-service-api/src/test/java/com/asterixcode/userserviceapi/UserServiceApiApplicationTests.java
+++ user-service-api/src/test/java/com/asterixcode/userserviceapi/UserServiceApiApplicationTests.java
@@ -1,11 +1,14 @@
 package com.asterixcode.userserviceapi;
 
+import com.asterixcode.userserviceapi.containers.MongoDBTestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@ActiveProfiles("utest")
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = MongoDBTestContainerConfig.class)
+@ActiveProfiles("it")
 class UserServiceApiApplicationTests {
 
   @Test
