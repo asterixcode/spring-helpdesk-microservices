3ee73bbc84d335a789962958dbd7ffd329d6765e
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-21 13:01:14 +0200

    test: rename unit test profile

--- user-service-api/src/test/java/com/asterixcode/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/com/asterixcode/userserviceapi/service/UserServiceTest.java
@@ -24,7 +24,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("utest")
 class UserServiceTest {
 
   @InjectMocks
