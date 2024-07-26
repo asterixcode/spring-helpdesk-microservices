10880a84aaed6ab0b0690b5afa7f530c6931176d
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-26 23:12:31 +0200

    feat: add exception handlers

--- order-service-api/src/main/java/com/asterixcode/orderserviceapi/controller/ControllerExceptionHandler.java
+++ order-service-api/src/main/java/com/asterixcode/orderserviceapi/controller/ControllerExceptionHandler.java
@@ -1,11 +1,22 @@
 package com.asterixcode.orderserviceapi.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import models.exceptions.GenericFeignException;
+import models.exceptions.ResourceNotFoundException;
+import models.exceptions.StandardError;
+import models.exceptions.ValidationError;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
@@ -13,5 +24,53 @@ public class ControllerExceptionHandler {
   @ExceptionHandler(GenericFeignException.class)
   ResponseEntity<Map> handleGenericFeignException(GenericFeignException ex) {
     return ResponseEntity.status(ex.getStatus()).body(ex.getError());
+  }
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  ResponseEntity<StandardError> handleResourceNotFoundException(
+          final ResourceNotFoundException ex, final HttpServletRequest request) {
+    return ResponseEntity.status(NOT_FOUND)
+            .body(
+                    StandardError.builder()
+                            .timestamp(now())
+                            .status(NOT_FOUND.value())
+                            .error(NOT_FOUND.getReasonPhrase())
+                            .message(ex.getMessage())
+                            .path(request.getRequestURI())
+                            .build());
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  ResponseEntity<StandardError> handleDataIntegrityViolationException(
+          final DataIntegrityViolationException ex, final HttpServletRequest request) {
+    return ResponseEntity.status(CONFLICT)
+            .body(
+                    StandardError.builder()
+                            .timestamp(now())
+                            .status(CONFLICT.value())
+                            .error(CONFLICT.getReasonPhrase())
+                            .message(ex.getMessage())
+                            .path(request.getRequestURI())
+                            .build());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  ResponseEntity<ValidationError> handleMethodArgumentNotValidException(
+          final MethodArgumentNotValidException ex, final HttpServletRequest request) {
+    var errors =
+            ValidationError.builder()
+                    .timestamp(now())
+                    .status(BAD_REQUEST.value())
+                    .error("Validation Error")
+                    .message("Exception in validation fields")
+                    .path(request.getRequestURI())
+                    .errors(new ArrayList<>())
+                    .build();
+
+    for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+      errors.addError(fieldError.getField(), fieldError.getDefaultMessage());
+    }
+
+    return ResponseEntity.badRequest().body(errors);
   }
 }
