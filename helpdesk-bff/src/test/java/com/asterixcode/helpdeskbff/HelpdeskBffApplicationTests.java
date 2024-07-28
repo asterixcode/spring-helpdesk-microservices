6103777024f0fd5fbf5c3e899935c4fc63d32d03
lucas.saldanha <lk.saldanha@gmail.com>
2024-07-28 11:26:14 +0200

    chore: start helpdesk bff project

---
+++ helpdesk-bff/src/test/java/com/asterixcode/helpdeskbff/HelpdeskBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asterixcode.helpdeskbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBffApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
