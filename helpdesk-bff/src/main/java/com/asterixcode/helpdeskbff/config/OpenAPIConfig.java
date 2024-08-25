515a908c6af6f69eeb8d45ced7bafe18ca176a12
lucas.saldanha <lk.saldanha@gmail.com>
2024-08-25 08:48:29 +0200

    feat: add swagger security config

--- helpdesk-bff/src/main/java/com/asterixcode/helpdeskbff/config/OpenAPIConfig.java
+++ helpdesk-bff/src/main/java/com/asterixcode/helpdeskbff/config/OpenAPIConfig.java
@@ -1,7 +1,10 @@
 package com.asterixcode.helpdeskbff.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,16 @@ public class OpenAPIConfig {
       @Value("${springdoc.open-api.info.title}") String title,
       @Value("${springdoc.open-api.info.description}") String description,
       @Value("${springdoc.open-api.info.version}") String version) {
-    return new OpenAPI().info(new Info().title(title).description(description).version(version));
+    return new OpenAPI()
+        .info(new Info().title(title).description(description).version(version))
+        .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "bearer-jwt",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
   }
 }
